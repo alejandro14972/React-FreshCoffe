ccea8cf0a45e8ea291146c371784e5ee6dfdd4db
Alejandro <agoal0@gmail.com>
2024-10-19 16:57:12 +0200

    uso de context 46

--- src/context/quioscoProvider.jsx
+++ src/context/quioscoProvider.jsx
@@ -1,15 +1,28 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
+import { categorias as categoriasDB } from "../data/categorias";
 
 const quioscoContext = createContext();
 
-const QuioscoProvider = ({children}) => {
+const QuioscoProvider = ({ children }) => {
 
-    const autenticado = true; 
+    const [categorias, setCategoria] = useState(categoriasDB);
+
+    const [categoriaActual, setCategoriaActual] = useState(categorias[0]);
+
+    const handleClickCategoria = id => {
+        const categoria = categorias.filter(categoria => categoria.id === id)[0]
+        console.log(categoria.nombre);
+        setCategoriaActual(categoria);
+    }
+
+    console.log(categoriaActual);
 
     return (
         <quioscoContext.Provider
             value={{
-                autenticado 
+                categorias,
+                categoriaActual,
+                handleClickCategoria
             }}
         >
             {children}
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,31 +1,27 @@
 import Producto from "../components/Producto";
-import { productos } from "../data/productos";
+import { productos as data } from "../data/productos";
 import useQuiosco from "../hooks/useQuiosco";
 
 export default function Inicio() {
+  const { categoriaActual } = useQuiosco();
 
-  const {autenticado}= useQuiosco();
-
-
-  console.log(autenticado);
-
+  // Usar filter con una función callback para filtrar los productos por categoria_id
+  const productos = data.filter(producto => producto.categoria_id === categoriaActual.id);
 
   return (
     <div>
-      <h1 className="text-4xl font-black"></h1>
+      <h1 className="text-4xl font-black">{categoriaActual.nombre}</h1>
       <p className="text-2xl my-10">
         Elige y personaliza tu pedido
       </p>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-2 xl:grid-cols-3">
-     
-        {productos.map(producto=>(
+        {productos.map(producto => (
           <Producto 
             key={producto.id}
             producto={producto}
           />
         ))}
-
       </div>
     </div>
-  )
+  );
 }
