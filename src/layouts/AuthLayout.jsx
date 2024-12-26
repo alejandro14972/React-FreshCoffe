772b5c105dc530cc592ffc60924fda4d05b534cf
Alejandro <pfu09125@educastur.es>
2024-12-26 23:14:36 +0100

    FIX authlayout

--- src/layouts/AuthLayout.jsx
+++ src/layouts/AuthLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 
-export default function authLayout() {
+export default function AuthLayout() {
   return (
     <main className="max-w-4xl m-auto mt-10 md:mt-28 flex flex-col md:flex-row items-center">
         <img src="../img/logo.svg" alt="" className="max-w-xs mr-9"/>
